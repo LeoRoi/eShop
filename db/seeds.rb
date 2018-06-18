bb62ac877864533893bb53e2d2447384f725ae40
Claudio Vindimian <s0558279@htw-berlin.de>
2018-06-18 19:46:32 +0200

    Table ersetz + render

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,30 +4,22 @@ Product.delete_all
 
 Product.create!(title: 'Not even final form',
                 description:
-                  %{<p>
-  	<em>Powerful, Effective, and Efficient</em>
-  	Civility vicinity graceful is it at. Improve up at to on mention perhaps raising. Way building not get formerly her peculiar. Up uncommonly prosperous sentiments simplicity acceptance to so. Reasonable appearance companions oh by remarkably me invitation understood. Pursuit elderly ask perhaps all.
-
-  	</p>},
+                  %{Powerful, Effective, and Efficient
+  	Civility vicinity graceful is it at. Improve up at to on mention perhaps raising. Way building not get formerly her peculiar. Up uncommonly prosperous sentiments simplicity acceptance to so. Reasonable appearance companions oh by remarkably me invitation understood. Pursuit elderly ask perhaps all.},
                 image_url: 'http://i0.kym-cdn.com/entries/icons/facebook/000/011/220/24219235.jpg',
-                price: 459.00)
+                price: 459.66)
 # . . .
 Product.create!(title: 'Need a raise',
                 description:
-                  %{<p>
-  	<em>My wife had nine children</em>
-  	By in no ecstatic wondered disposal my speaking. Direct wholly valley or uneasy it at really. Sir wish like said dull and need make. Sportsman one bed departure rapturous situation disposing his. Off say yet ample ten ought hence. Depending in newspaper an september do existence strangers. Total great saw water had mirth happy new. Projecting pianoforte no of partiality is on. Nay besides joy society him totally six.
-  	</p>},
+                  %{My wife had nine children
+  	By in no ecstatic wondered disposal my speaking. Direct wholly valley or uneasy it at really. Sir wish like said dull and need make. Sportsman one bed departure rapturous situation disposing his. Off say yet ample ten ought hence. Depending in newspaper an september do existence strangers. Total great saw water had mirth happy new. Projecting pianoforte no of partiality is on. Nay besides joy society him totally six.},
                 image_url: 'https://dvtest.co.uk/wildcard3/wp-content/uploads/2015/01/need-a-raise_o_996287.jpg',
-                price: 300000.00)
+                price: 3000.55)
 # . . .
 
 Product.create!(title: 'What is meme',
                 description:
-                  %{<p>
-  	<em>Show me</em>
-  	Why end might ask civil again spoil. She dinner she our horses depend. Remember at children by reserved to vicinity. In affronting unreserved delightful simplicity ye. Law own advantage furniture continual sweetness bed agreeable perpetual. Oh song well four only head busy it. Afford son she had lively living. Tastes lovers myself too formal season our valley boy. Lived it their their walls might to by young.
-
-  	</p>},
+                  %{Show me
+  	Why end might ask civil again spoil. She dinner she our horses depend. Remember at children by reserved to vicinity. In affronting unreserved delightful simplicity ye. Law own advantage furniture continual sweetness bed agreeable perpetual. Oh song well four only head busy it. Afford son she had lively living. Tastes lovers myself too formal season our valley boy. Lived it their their walls might to by young.},
                 image_url: 'http://www.digitalwiki.de/wp-content/uploads/2017/02/That-would-be-great-meme.jpg',
                 price: 4.99)
