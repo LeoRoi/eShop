65ce7089da17be0494b030f5c006aabd53aad24b
Claudio Vindimian <s0558279@htw-berlin.de>
2018-06-19 14:14:27 +0200

    validation %r

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,7 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix,
+    with: %r/\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix,
     message: 'must be a URL for GIF, JPG or PNG image location!'
   }
 end
-
-
-
-
