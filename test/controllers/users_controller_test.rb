4fb0659df3c045b28d3109ca62f7c2ca80d028bf
leoroi <kuzne4eg@gmail.com>
2018-09-12 09:58:59 +0200

    .keep files returned

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    @user2 = users(:two)
   end
 
   test "should get index" do
@@ -49,6 +50,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should destroy user" do
     assert_difference('User.count', -1) do
       delete user_url(@user)
+    end
+
+    assert_redirected_to users_url
+  end
+
+  test "should not destroy last user" do
+    assert_difference('User.count', -1) do
+      delete user_url(@user)
+      delete user_url(@user2)
     end
 
     assert_redirected_to users_url
