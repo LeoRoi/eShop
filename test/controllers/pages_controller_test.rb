1433ef9701b7f51ea118c816b01944e45fba3e70
LeoRoi <kuzne4eg@gmail.com>
2018-10-03 15:40:08 +0200

    bilingual home with carousel

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get pages_show_url
+  test "should get home" do
+    get home_url
     assert_response :success
   end
 
